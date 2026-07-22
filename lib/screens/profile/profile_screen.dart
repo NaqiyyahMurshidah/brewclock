a0b41a467d9036d48073726e3c219691c3abfd64
akisinthemood <balqis.jamhari@student.iium.edu.my>
2026-07-22 18:47:05 +0800

    profile buruk 1

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import '../../widgets/profile/profile_page.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF3B2A20),
-      body: Center(child: Text("Profile")),
-    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Profile'),
+          backgroundColor: const Color(0xFF3B2A20),
+          elevation: 0,
+        ),
+        body: const ProfilePage(),
+        ),
+      );
   }
 }
